3931e3d42d49101e5027711ea826ca7beee779e0
ziy-offc <samsonkate0023@gmail.com>
2026-03-26 14:06:58 +0700

    Delete Types/Chat.js before ZIP upload

--- Types/Chat.js
+++
@@ -1,9 +0,0 @@
-//=======================================================//
-export const ALL_WA_PATCH_NAMES = [
-  "critical_block",
-  "critical_unblock_low",
-  "regular_high",
-  "regular_low",
-  "regular"
-];
-//=======================================================//
