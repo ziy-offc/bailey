32a2d83055dd9023a01c2e99c907c63f883dd4f8
ziy-offc <samsonkate0023@gmail.com>
2026-03-26 13:59:11 +0700

    Upload Types/Chat.js from ZIP extraction

---
+++ Types/Chat.js
@@ -0,0 +1,9 @@
+//=======================================================//
+export const ALL_WA_PATCH_NAMES = [
+  "critical_block",
+  "critical_unblock_low",
+  "regular_high",
+  "regular_low",
+  "regular"
+];
+//=======================================================//
