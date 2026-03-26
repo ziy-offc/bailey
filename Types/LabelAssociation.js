b933c13708c9cece303bd984963e01ba06b5009f
ziy-offc <samsonkate0023@gmail.com>
2026-03-26 14:07:03 +0700

    Delete Types/LabelAssociation.js before ZIP upload

--- Types/LabelAssociation.js
+++
@@ -1,7 +0,0 @@
-//=======================================================//
-export var LabelAssociationType;
-(function (LabelAssociationType) {
-  LabelAssociationType["Chat"] = "label_jid";
-  LabelAssociationType["Message"] = "label_message";
-})(LabelAssociationType || (LabelAssociationType = {}));
-//=======================================================//
