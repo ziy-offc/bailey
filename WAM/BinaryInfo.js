49baa332cd84a1e6eff8e4d5e395c9e09805799f
ziy-offc <samsonkate0023@gmail.com>
2026-03-26 13:59:42 +0700

    Upload WAM/BinaryInfo.js from ZIP extraction

---
+++ WAM/BinaryInfo.js
@@ -0,0 +1,11 @@
+//=======================================================//
+export class BinaryInfo {
+  constructor(options = {}) {
+    this.protocolVersion = 5;
+    this.sequence = 0;
+    this.events = [];
+    this.buffer = [];
+    Object.assign(this, options);
+  }
+}
+//=======================================================//
